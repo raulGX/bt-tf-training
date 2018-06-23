4b78b57b34db073131a2823234f9a9512cb27730
raulpopovici <raul.popovici95@gmail.com>
2018-06-23 12:17:50 +0300

    code cleanup

--- scripts/test.py
+++ scripts/test.py
@@ -26,8 +26,6 @@ def read_tensor_from_image_file(file_vector,
     input_name = "file_reader"
     output_name = "normalized"
 
-    # image_reader = tf.image.decode_image(file_vector)
-    # float_caster = tf.cast(image_reader, tf.float32)
     dims_expander = tf.expand_dims(file_vector, 0)
     resized = tf.image.resize_bilinear(
         dims_expander, [input_height, input_width])
@@ -36,6 +34,14 @@ def read_tensor_from_image_file(file_vector,
     result = sess.run(normalized)
 
     return result
+
+
+def is_obstacle(label):
+    ground_labels = ['grass', 'sidewalk', 'street']
+    if label in ground_labels:
+        return False
+    else:
+        return True
 
 
 if __name__ == "__main__":
@@ -63,6 +69,9 @@ if __name__ == "__main__":
     labels = load_labels(label_file)
     wrong = 0
     count = 0
+    missmatch = 0
+    false_positive = 0
+    false_negative = 0
     with tf.Session(graph=graph) as sess:
         for label in data:
             if label not in labels:
@@ -76,11 +85,23 @@ if __name__ == "__main__":
                                    {input_operation.outputs[0]: imageTensor})
                 results = np.squeeze(results)
                 top_k = results.argsort()[-5:][::-1]
-                if labels[top_k[0]] != label:
+                top_1 = labels[top_k[0]]
+                if top_1 != label:
                     wrong = wrong + 1
-                    print(wrong, label, labels[top_k[0]])
+                    print(wrong, label, top_1)
+                    if is_obstacle(top_1) != is_obstacle(label):
+                        missmatch = missmatch + 1
+                        if is_obstacle(top_1):
+                            false_positive = false_positive + 1
+                        else:
+                            false_negative = false_negative + 1
+                        print('missmatch obstacle', missmatch)
 
             print('done', label)
 
     print(wrong, count)
     print('bad accuracy', wrong/count)
+    print(missmatch, count)
+    print('missmath obstacle', 1 - missmatch/count)
+    print('false negatives', false_negative, false_negative / count)
+    print('false positives', false_positive, false_positive / count)
